2164eb58a2f8078454fa1d2af5d5b91aa98c6c8c
adidal <hello@cdstm.ch>
2025-02-21 10:34:01 +0000

    Basic transcription service created ; replaced the default Android one because it cannot do diarisation.

--- liveprotector/macos/Flutter/GeneratedPluginRegistrant.swift
+++ liveprotector/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import speech_to_text
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
